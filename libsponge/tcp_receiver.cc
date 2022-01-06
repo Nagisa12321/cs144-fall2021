b38922643f5fb7fdf562ee5318aaa64b590753ae
jt <1216414009@qq.com>
2022-01-06 14:41:00 +0800

    feat: ok to write tcp_receiver but 3 tests not passed

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -5,15 +5,41 @@
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    // •
+    // Set the Initial Sequence Number if necessary. The sequence number of the first-
+    // arriving segment that has the SYN flag set is the initial sequence number. You’ll want
+    // to keep track of that in order to keep converting between 32-bit wrapped seqnos/acknos
+    // and their absolute equivalents. (Note that the SYN flag is just one flag in the header.
+    // The same segment could also carry data and could even have the FIN flag set.)
+    if (seg.header().syn && !_isn_setted) {
+        _isn_setted = true;
+        _isn = seg.header().seqno;
+    }
+    // •
+    // Push any data, or end-of-stream marker, to the StreamReassembler. If the
+    // FIN flag is set in a TCPSegment’s header, that means that the last byte of the payload
+    // is the last byte of the entire stream. Remember that the StreamReassembler expects
+    // stream indexes starting at zero; you will have to unwrap the seqnos to produce these.
+    // 
+    if (seg.payload().size() != 0) {
+        uint64_t checkpoint = _reassembler.stream_out().bytes_read();
+        uint64_t index = unwrap(seg.header().seqno, _isn, checkpoint) - 1;
+        string data = seg.payload().copy();
+        _reassembler.push_substring(data, index, false);
+    } 
+    if (seg.header().fin) 
+        _reassembler.stream_out().end_input();
+
+    _seq_len += seg.length_in_sequence_space() - seg.payload().size();
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const { 
+    if (!_isn_setted) return optional<WrappingInt32>();
+    else return optional<WrappingInt32>(_reassembler.stream_out().bytes_written() + _isn.raw_value() + _seq_len);
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const 
+    { return _capacity - _reassembler.stream_out().buffer_size(); }
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,12 +20,22 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
-  public:
+    WrappingInt32 _isn; 
+    bool _isn_setted;
+	size_t _seq_len;
+
+public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) 
+    	: _reassembler(capacity), 
+		  _capacity(capacity),
+		  _isn(0),
+		  _isn_setted(false),
+		  _seq_len(0)
+		  {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
